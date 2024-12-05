64b316376a5d216296cb35e8df66888816ce0a81
Hemant <hemantnawghare13@gmail.com>
2024-12-05 07:19:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('signup-button').addEventListener('click', function() {
+    alert('Thank you for signing up! We will get in touch with you soon.');
+});
+
+
+document.querySelector('.carousel-button').addEventListener('click', () => {
+    alert('Carousel button clicked! Add functionality here.');
+});
